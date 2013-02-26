cfb0d72eba6eeb61c5d9b283383b9cf7972f6777
Vincent Rabaud <vrabaud@willowgarage.com>
2013-02-26 12:45:33 -0800

    Rebase from 'debian/fuerte/object_recognition_capture'

--- python/object_recognition_capture/orb_capture.py
+++ python/object_recognition_capture/orb_capture.py
@@ -1,10 +1,12 @@
-import ecto
+"""
+Module defining a few BlackBoxes for textured plane tracking
+"""
 from ecto import BlackBoxCellInfo, BlackBoxForward
+from ecto_opencv.calib import Select3d, Select3dRegion, PoseDrawer, TransformCompose
+from ecto_opencv.features2d import FASTFeature, ORB, Matcher, MatchRefinementHSvd, DrawMatches, KeypointsToMat, \
+    LSHMatcher
 from ecto_opencv.highgui import imshow, FPSDrawer, MatReader, imread
-from ecto_opencv.features2d import FASTFeature, ORB, Matcher, \
-    MatchRefinementHSvd, DrawMatches, KeypointsToMat
-from ecto_opencv.calib import Select3d, Select3dRegion, PlaneFitter, PoseDrawer, TransformCompose
-from ecto_opencv.features2d import LSHMatcher
+import ecto
 
 class FeatureFinder(ecto.BlackBox):
     @classmethod
@@ -47,7 +49,7 @@ class TemplateLoader(ecto.BlackBox):
         return {'points': BlackBoxCellInfo(MatReader, {'filename': '%s/points.yaml' % p.directory}),
                 'points3d': BlackBoxCellInfo(MatReader, {'filename': '%s/points3d.yaml' % p.directory}),
                 'descriptors': BlackBoxCellInfo(MatReader, {'filename': '%s/descriptors.yaml' % p.directory}),
-                'R': BlackBoxCellInfo(MatReader, {'R': '%s/R.yaml' % p.directory}),
+                'R': BlackBoxCellInfo(MatReader, {'filename': '%s/R.yaml' % p.directory}),
                 'T': BlackBoxCellInfo(MatReader, {'filename': '%s/T.yaml' % p.directory}),
                 'image': BlackBoxCellInfo(imread, {'image_file=': '%s/train.png' % p.directory})}
 
@@ -66,27 +68,6 @@ class TemplateLoader(ecto.BlackBox):
 
     def connections(self, _p):
         return [self.points, self.points3d, self.descriptors, self.R, self.T, self.image]
-
-class PlaneEstimator(ecto.BlackBox):
-    #find a plane in the center region of the image.
-    @classmethod
-    def declare_cells(cls, _p):
-        return {'flag': BlackBoxCellInfo(ecto.Passthrough),
-                'plane_fitter': BlackBoxCellInfo(PlaneFitter),
-                'region': BlackBoxCellInfo(Select3dRegion)}
-
-    @classmethod
-    def declare_forwards(self, _p):
-        return ({'region': 'all'},
-                {'region': 'all', 'flag': [BlackBoxForward('in','set')]},
-                {'plane_fitter': 'all'})
-
-    def connections(self, _p):
-        return [ self.region['points3d'] >> self.plane_fitter['points3d'] ]
-#
-#class OrbTemplate(ecto.BlackBox):
-#    '''Takes a template image, computes orb, and saves it.'''
-#    
 
 class OrbPoseEstimator(ecto.BlackBox):
     '''Estimates the pose of an ORB based template.
@@ -110,7 +91,8 @@ class OrbPoseEstimator(ecto.BlackBox):
         p.declare('show_matches', 'Display the matches.', False)
         p.declare('use_lsh', 'Use lsh for matching instead of brute force.', True)
 
-    def declare_forwards(self, _p):
+    @staticmethod
+    def declare_forwards(_p):
         #inputs
         p = {'lsh': 'all', 'orb': 'all', 'pose_estimation': 'all'}
         i = {}
